d62487ae55609c28f2633ab53dd9b701d1509810
daei <daei0721@yahoo.co.jp>
2019-12-10 23:59:07 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
 end
