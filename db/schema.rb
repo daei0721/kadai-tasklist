17ce9e8fddc40b631e4fd43b79f5afbf8a2efccb
daei <daei0721@yahoo.co.jp>
2019-12-11 12:44:43 +0000

    kadai2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_131321) do
+ActiveRecord::Schema.define(version: 2019_12_11_120552) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
